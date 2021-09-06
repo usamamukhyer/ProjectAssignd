b2362308db51e857a9ca5f032248a1463e9d995b
Usamamukhyer <usamamukhyer@gmail.com>
2021-09-06 05:37:56 -0700

    My Project after working on the the crud operation of create developer witout toaster

--- ProjectAssigned/ProjectAssigned/Controllers/CreateDeveloperController.cs
+++ ProjectAssigned/ProjectAssigned/Controllers/CreateDeveloperController.cs
@@ -5,13 +5,19 @@ using System.Web;
 using System.Web.Mvc;
 using System.IO;
 using ProjectAssigned.Models;
+using System.Net;
+using System.Data.Entity;
 
 namespace ProjectAssigned.Controllers
 {
     public class CreateDeveloperController : Controller
     {
+       
 
         ProjectAssignedEntities db = new ProjectAssignedEntities();
+        string extension;
+        string fileName;
+        string uploadUrl;
         // GET: CreateDeveloper
 
         public ActionResult DeveloperList()
@@ -24,75 +30,203 @@ namespace ProjectAssigned.Controllers
         }
          
         [HttpPost]
-        public ActionResult AddDeveloper(HttpPostedFileBase Dp, HttpPostedFileBase cvdata   , CreateDeveloper create)
+        public ActionResult AddDeveloper( CreateDeveloper create,HttpPostedFileBase Dp, HttpPostedFileBase cvdata   )
         {
+            
+
             try
             {
-                
-                    if(Dp!=null)
+                //upload cv file
+                if (cvdata != null )
+                {
+                    extension = Path.GetExtension(cvdata.FileName);
+                    if (extension.ToLower() == ".doc" || extension.ToLower() == ".docx" || extension.ToLower() == ".txt")
                     {
-                        string dpname = Path.GetFileName(Dp.FileName);
-                        string _dpname = DateTime.Now.ToString("yymmssfff") + dpname;
-                        string extension = Path.GetFileName(Dp.FileName);
-                        string path = Path.Combine(Server.MapPath("~/ Content/Filesdata/Images"),_dpname);
-                        create.Image = "~/ Content/Filesdata/Images" + _dpname;
-                        if(extension.ToLower()==".jpg"|| extension.ToLower() == ".jpeg" || extension.ToLower() == ".png")
+                        if (cvdata.ContentLength < 1000000)
                         {
-                            if (Dp.ContentLength<2000000)
-                            {
-                                Dp.SaveAs(path);
-                                
-                            }
-                            else
-                            {
-                                ViewBag.message = "file is to much long";
-                            }
+                            fileName = Guid.NewGuid().ToString() + extension;
+                            uploadUrl = Server.MapPath("~/Content/Filesdata/Cv/");
+                            create.Cv = "~/Content/Filesdata/Cv/" + fileName;
+                            cvdata.SaveAs(Path.Combine(uploadUrl, fileName));
                         }
-
-
+                        else
+                        {
+                            ViewBag.mess = "file size must be less than 2 mb";
+                            return View(create);
+                        }
                     }
-                    if (cvdata != null)
+                    else
                     {
-                        string data = Path.GetFileName(cvdata.FileName);
-                        string _dataname = DateTime.Now.ToString("yymmssfff") + data;
-                        string extension = Path.GetFileName(cvdata.FileName);
-                        string path = Path.Combine(Server.MapPath("~/ Content/Filesdata/Cv"), _dataname);
-                        create.Cv = "~/ Content/Filesdata/Cv" + _dataname; 
-                        if (extension.ToLower() == ".txt" || extension.ToLower() == ".doc" || extension.ToLower() == ".pdf")
-                        {
-                            if (cvdata.ContentLength < 2000000)
-                            {
-                                cvdata.SaveAs(path);
-
-                            }
-                            else
-                            {
-                                ViewBag.message = "file is to much long";
-                            }
-                        }
-
-
-                    
-                    db.CreateDevelopers.Add(create);
-                    db.SaveChanges();
-                    ModelState.Clear();
-
-                    return RedirectToAction(" DeveloperList","CreateDeveloper");
-                    
-
+                        ViewBag.Mess = "file must doc,docx or .txt";
+                        return View(create);
+                    }
                 }
+                //Dp Upload code
+                if (Dp!=null)
+                {
+                    extension =  Path.GetExtension(Dp.FileName);
+                    if (extension.ToLower() == ".png" || extension.ToLower() == ".jpg" || extension.ToLower() == ".jpeg")
+                    {
+                        if (Dp.ContentLength < 500000)
+                        { 
+                          fileName = Guid.NewGuid().ToString() + extension;
+                          uploadUrl = Server.MapPath("~/Content/Filesdata/Images/");
+                         create.Image = "~/Content/Filesdata/Images/" + fileName;
+                         Dp.SaveAs(Path.Combine(uploadUrl, fileName));
 
-            }
-            catch(Exception ex)
-            {
+                        }
+                        else
+                        {
+                            ViewBag.message = "Picture size must be less than 2 mb";
+                            return View(" AddDeveloper");
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Picture must be png, jpg or jpeg";
+                        return View( create);
+                    }
+                }
                 
-                ModelState.AddModelError("", "somthing went wrong");
+                //add record in the Db
+                db.CreateDevelopers.Add(create);
+                db.SaveChanges();
+                ModelState.Clear();
+                return RedirectToAction("DeveloperList");
             }
-
-            return View("AddDeveloper");
+            catch(Exception)
+            {
+                ModelState.AddModelError("", "hi this is not good approach");
+            }
+            return View(create);
         }
 
 
-       
+
+        //Edit Get
+        public ActionResult EditDeveloper(int?id)
+        { 
+              CreateDeveloper create = new CreateDeveloper();
+             
+        
+            //if(id==null)
+            //{
+            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            //}
+            if (id != null)
+            {
+              
+            create = db.CreateDevelopers.Where(model => model.Developer_Id == id).FirstOrDefault<CreateDeveloper>();
+               
+            }
+
+            return View(create);
+        }
+        [HttpPost]
+        public ActionResult EditDeveloper(CreateDeveloper create, HttpPostedFileBase Dp, HttpPostedFileBase cvdata)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    //upload cv file
+                    if (cvdata != null)
+                    {
+                        extension = Path.GetExtension(cvdata.FileName);
+                        if (extension.ToLower() == ".doc" || extension.ToLower() == ".docx" || extension.ToLower() == ".txt")
+                        {
+                            if (cvdata.ContentLength < 1000000)
+                            {
+                                fileName = Guid.NewGuid().ToString() + extension;
+                                uploadUrl = Server.MapPath("~/Content/Filesdata/Cv/");
+                                create.Cv = "~/Content/Filesdata/CV/" + fileName;
+                                if (!String.IsNullOrEmpty(create.Cv))
+                                {
+                                    if (System.IO.File.Exists(uploadUrl  + create.Cv))
+                                    {
+                                        System.IO.File.Delete(uploadUrl  + create.Cv);
+
+                                    }
+                                }
+                                cvdata.SaveAs(Path.Combine(uploadUrl, fileName));
+                               
+                            }
+                            else
+                            {
+                                ViewBag.mess = "file size must be less than 2 mb";
+                                return View(create);
+                            }
+                        }
+                        else
+                        {
+                            ViewBag.Mess = "file must doc,docx or .txt";
+                            return View(create);
+                        }
+                    }
+                    //Dp Upload code
+                    if (Dp != null)
+                    {
+                        extension = Path.GetExtension(Dp.FileName);
+                        if (extension.ToLower() == ".png" || extension.ToLower() == ".jpg" || extension.ToLower() == ".jpeg")
+                        {
+                            if (Dp.ContentLength < 500000)
+                            {
+                                
+                                uploadUrl = Server.MapPath("~/Content/Filesdata/Images/");
+                               
+                                if (!String.IsNullOrEmpty(create.Image))
+                                {
+                                    if (System.IO.File.Exists(uploadUrl + "/" + create.Image))
+                                    {
+                                        System.IO.File.Delete(uploadUrl + "/" + create.Image);
+
+                                    }
+                                }
+                                fileName = Guid.NewGuid().ToString() + extension;
+                                create.Image = "~/Content/Filesdata/Images/" + fileName;
+                                Dp.SaveAs(Path.Combine(uploadUrl, fileName));
+
+                            }
+                            else
+                            {
+                                ViewBag.message = "Picture size must be less than 2 mb";
+                                return View(" AddDeveloper");
+                            }
+                        }
+                        else
+                        {
+                            ViewBag.Message = "Picture must be png, jpg or jpeg";
+                            return View(create);
+                        }
+                    }
+
+                    //add record in the Db
+                    db.Entry(create).State = EntityState.Modified;
+                    db.SaveChanges();
+                   
+                    return RedirectToAction("DeveloperList");
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "hi this is not good approach");
+                }
+            }
+            else
+            {
+                db.Entry(create).State = EntityState.Modified;
+                db.SaveChanges();
+
+            }
+            
+           return View(create);
+        }
+
+        public JsonResult IsUserExists(string Username, string UserPassword, string Email, string Phone)
+        {
+            //check if any of the UserName matches the UserName specified in the Parameter using the ANY extension method.  
+            return Json(!db.CreateDevelopers.Any(x => x.Username == Username || x.UserPassword== UserPassword || x.Email== Email || x.Phone == Phone), JsonRequestBehavior.AllowGet);
+        }
+
+
     }
 }
--- ProjectAssigned/ProjectAssigned/Models/MyProjectMetadata.cs
+++ ProjectAssigned/ProjectAssigned/Models/MyProjectMetadata.cs
@@ -3,36 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
 namespace ProjectAssigned.Models
 {
     public class CreateDeveloperMetadata
     {
-        
+      
         public int Developer_Id { get; set; }
-        [Required(ErrorMessage ="this First name is required")]
+        [Required(ErrorMessage = "this First name is required")]
+        [StringLength(8, ErrorMessage = "Must be between {2} and {1} characters", MinimumLength = 2)]
         public string Firstname { get; set; }
         [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(8, ErrorMessage = "Must be between {2} and {1} characters", MinimumLength = 2)]
         public string Lastname { get; set; }
         [Required(ErrorMessage = "image is required")]
         public string Image { get; set; }
         [Required(ErrorMessage = "Designation is required")]
+        [StringLength(10, ErrorMessage = "Must be between {2} and {1} characters", MinimumLength = 6)]
         public string Designation { get; set; }
         [Required(ErrorMessage = "Date time is required")]
         public System.DateTime Joindate { get; set; }
         [Required(ErrorMessage = "salary is required")]
+        [StringLength(15, ErrorMessage = "Must be between {2} and {1} characters", MinimumLength = 1)]
+        [Range(300, 10000000, ErrorMessage = "Salary must be between 300 and 10000000")]
         public string Salary { get; set; }
+
+
         [Required(ErrorMessage = "email is required")]
-        [DataType(DataType.EmailAddress,ErrorMessage ="must b include @")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "must b include @")]
+        [StringLength(30, ErrorMessage = "Must be between {2} and {1} characters", MinimumLength = 8)]
+        [Remote("IsUserExists", "CreateDeveloper", ErrorMessage = "Email is already taken")]
+
         public string Email { get; set; }
         [Required(ErrorMessage = "Address is required")]
+        [StringLength(100, ErrorMessage = "Must be between {2} and {1} characters", MinimumLength = 10)]
         public string Address { get; set; }
         [Required(ErrorMessage = "Phone is required")]
+        
+        [Range(0, 20, ErrorMessage = "Phone must be between 0 and 20")]
+        [Remote("IsUserExists", "CreateDeveloper", ErrorMessage = "Phone is already taken")]
         public string Phone { get; set; }
+        [StringLength(20, ErrorMessage = "Must be between {2} and {1} characters", MinimumLength = 6)]
         [Required(ErrorMessage = "Experience is required")]
         public string Experience { get; set; }
         [Required(ErrorMessage = "cv is required")]
         public string Cv { get; set; }
         [Required(ErrorMessage = "Bio is required")]
+        [StringLength(200, ErrorMessage = "Must be between {2} and {1} characters", MinimumLength = 10)]
         public string Bio { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(20, ErrorMessage = "Must be between {2} and {1} characters", MinimumLength = 4)]
+        [Remote("IsUserExists", "CreateDeveloper", ErrorMessage = "User name is already taken")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(10, ErrorMessage = "Must be between {2} and {1} characters", MinimumLength = 6)]
+        [Remote("IsUserExists", "CreateDeveloper", ErrorMessage = "Password is already taken")]
+        public string UserPassword { get; set; }
+       
+
     }
+
+    
+    
 }
